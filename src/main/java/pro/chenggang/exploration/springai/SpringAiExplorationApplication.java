343fdb542f1ac39ba8ba6593a595d10d38c820e0
Gang Cheng <chenggangpro@gmail.com>
2024-02-26 15:08:29 +0800

    init project

---
+++ src/main/java/pro/chenggang/exploration/springai/SpringAiExplorationApplication.java
@@ -0,0 +1,13 @@
+package pro.chenggang.exploration.springai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiExplorationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiExplorationApplication.class, args);
+    }
+
+}
